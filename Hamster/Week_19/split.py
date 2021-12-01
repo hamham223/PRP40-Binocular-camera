1730a95880616ba98f0ae86b091902afaebe9b64
niyinchen <niyinchen@sjtu.edu.cn>
2021-12-01 16:10:14 +0800

    new_graph

---
+++ Hamster/Week_19/split.py
@@ -0,0 +1,10 @@
+import cv2
+for i in range(1,6):
+    img=cv2.imread("Origin_"+str(i)+".jpg")
+    
+    i1=img[0:1520,0:1520]
+    i2=img[0:1520,1520:3040]
+    cv2.imwrite("Left_"+str(i)+".jpg",i1)
+    cv2.imwrite("Right_"+str(i)+".jpg",i2)
+    print(i1.shape)
+    print(i2.shape)
